66bc6a3aee29b68f82bb6bf911cc67d5b748fbf9
mohamedElnagar79 <mohamedelnaggar002@gmail.com>
2022-07-30 14:30:15 +0200

    login , sign up structure , and navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import KitchenDetails from "./pages/Kitchen/KitchenDetails";
+import NavBar from "./components/NavBar/NavBar";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
   return (
     <Router>
-      {/* <NavComponent /> */}
+      <NavBar />
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/home" exact component={Home} />
         <Route path="/kitchendetails/:id" component={KitchenDetails} />
-
+        <Route path="/login" component={SignIn} />
+        <Route path="/signUp" component={SignUp} />
+        <Route path="/" exact component={Home} />
         {/* <Route path="*" exact component={NotFound}/> */}
       </Switch>
     </Router>
