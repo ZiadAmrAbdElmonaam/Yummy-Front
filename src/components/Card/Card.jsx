1f9a06b4f214cf25d8c44eb907845fd29ff235b4
mohamedElnagar79 <mohamedelnaggar002@gmail.com>
2022-07-31 17:57:34 +0200

    handel img

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Card.css";
+// import "../../../../Yummy-Graduation/avatars/images/"
+
 function CardComponent({
   kitchen: { kitchenImage, kitchenName, kitchenStatus, _id },
 }) {
+  let imgUrl = "../../../../Yummy-Graduation";
   return (
     <>
       <div className="card h-100 w-100">
-        <img src={kitchenImage} className="card-img-top" alt="..." />
+        <img src="../../../../Yummy-Graduation/avatars/images/1659025147767.jpg" />
+        <img
+          src={imgUrl + kitchenImage}
+          className="card-img-top responsive-img"
+          alt="..."
+        />
         <div className="card-body">
           <h5 className="card-title">{kitchenName}</h5>
           <p className="card-text">{kitchenStatus}</p>
