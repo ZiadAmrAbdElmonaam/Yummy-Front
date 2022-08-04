0c07185d7534855dd458788335d4e9c17334f2f6
mohamedElnagar79 <mohamedelnaggar002@gmail.com>
2022-08-04 20:06:29 +0200

    fetch pilot orders

--- src/pages/Pilot/Pilot.jsx
+++ src/pages/Pilot/Pilot.jsx
@@ -1,50 +1,50 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axiosInstance from "../../Network/Config";
 import React, { useEffect, useState } from "react";
 import Loader from "../../components/Loader/Loader";
-import "./Pilot.css"
+import "./Pilot.css";
+import PilotOrders from "./PilotOrders";
 
 export default function Pilot() {
-    const [pilot, setPilot] = useState({});
-    // const [item, setItem] = useState([]);
-    let [isload, setIsLoad] = useState(true);
-  
-    let params = useParams();
-    console.log("params", params);
-    useEffect(() => {
-      axiosInstance
-        .get(`/pilotOrders/${params.id}`)
-        .then((res) => {
-          setPilot(res.data);
-          setIsLoad(false);
-        //   setItem(res.data.menuId.menuItems);
-        }
+  const [pilot, setPilot] = useState({});
+  // const [item, setItem] = useState([]);
+  let [isload, setIsLoad] = useState(true);
 
-        )
-        .catch((err) => {
-          setIsLoad(false);
-        });
-    }, []);
-    // console.log("nnn", item);
+  let params = useParams();
+  console.log("params", params);
+  useEffect(() => {
+    axiosInstance
+      .get(`/pilotOrders/${params.id}`)
+      .then((res) => {
+        setPilot(res.data);
+        setIsLoad(false);
+        //   setItem(res.data.menuId.menuItems);
+      })
+      .catch((err) => {
+        setIsLoad(false);
+      });
+  }, []);
+  // console.log("nnn", item);
 
   return (
     <>
-     {isload ? (
+      {isload ? (
         <Loader />
       ) : (
-      <>
-      <div className="row">
-      
-      <div className="col-12 ">
-        <h2 className="pilotHeader">
-        Welcome {pilot.pilotName}
-        </h2>
-      </div>
-      {/* <div className="row">0{pilot.pilotNumber}</div> */}
-      <div></div>
-      </div>
-      </>
-
+        <>
+          <div className="cover">
+            <div className="cover-info">
+              <h2 className="pilotHeader">Welcome {pilot.pilotName}</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Distinctio alias natus magni quod fuga ducimus adipisci velit
+                dolores doloremque, laboriosam omnis vero placeat asperiores
+                animi nemo at deleniti eius debitis?
+              </p>
+            </div>
+          </div>
+          <PilotOrders pilot={pilot} />
+        </>
       )}
     </>
   );
---
+++ src/pages/Pilot/PilotOrders.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./Pilot.css";
+
+export default function PilotOrders(props) {
+  console.log(props);
+  return (
+    <div className="container">
+      <h1 className="orders-head">My Orders</h1>
+      <div className="table-responsive">
+        <table className="table  table-hover my-5 ">
+          <thead className="table-warning ">
+            <tr>
+              <td>orders</td>
+              <td>kitchen name</td>
+              <td>kitchen zone</td>
+              <td>client name</td>
+              <td>client address</td>
+              <td>Item</td>
+              <td>pilot Order Status</td>
+              <td>total price</td>
+            </tr>
+          </thead>
+          <tbody>
+            {props.pilot.orders.map((order, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{order.kitchen.kitchenName}</td>
+                  <td>
+                    {order.kitchen.kitchenAddress.buildingNumber},{" "}
+                    {order.kitchen.kitchenAddress.street},{" "}
+                    {order.kitchen.kitchenAddress.zone}
+                  </td>
+                  <td>{order.userid.userFullName}</td>
+                  <td>
+                    {order.userid.userAddress.building},{" "}
+                    {order.userid.userAddress.street},{" "}
+                    {order.userid.userAddress.zone}
+                  </td>
+                  <td>
+                    <ul>
+                      {order.orderItems.map((item, index) => {
+                        return (
+                          <li key={index}>
+                            {item.itemName} : {item.itemPrice} <span>LE</span>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </td>
+                  <td>{order.pilotOrderStatus}</td>
+                  <td>
+                    {order.totalPrice} <span>LE</span>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
