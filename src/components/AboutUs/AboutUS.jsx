b890d83ea1053172cfb9b922799851902c8a5ad3
mohamedElnagar79 <mohamedelnaggar002@gmail.com>
2022-08-13 05:03:27 +0200

    add about us

--- src/components/AboutUs/AboutUS.jsx
+++ src/components/AboutUs/AboutUS.jsx
@@ -1,5 +1,23 @@
 import React from "react";
-
+import "./AboutUs.css";
 export default function AboutUS() {
-  return <div>AboutUS</div>;
+  return (
+    <div class="about-us">
+      <div class="about-top p-5">
+        <div class="top">
+          <h1>ABOUT US</h1>
+          <p>
+            Yummy is Online food ordering application, considered as an
+            increasingly used application in homemade food management. Just a
+            few years ago, people used to spend times to go restaurants junky
+            food . And after that, they would wait longer time to order their
+            food. There was no surprise that the food took too long to be
+            prepared. In short, the whole enjoyment of eating did eventually
+            result in boredom and tiredness for customers. We make it easy for
+            people to order and eat healthy homemade food.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
